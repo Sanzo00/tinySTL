67de82d6a5d5070a31925507bfee99e4f2175d1b
Sanzona <twonapom@gmail.com>
2020-08-10 15:19:44 +0100

    change name

---
+++ test/stl_test.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include "allocator/add.h"
+// #include "add.h"
+
+using namespace std;
+
+int main() {
+
+    cout << "hello world" << endl;
+
+	int a = 1, b = 2;
+	cout << "a + b = " << add(a, b) << endl;
+
+    return 0;
+}
+
