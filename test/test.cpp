1ed889e401e693e15e06a6898cf39b4458be7ad0
Sanzona <twonapom@gmail.com>
2020-08-10 12:26:08 +0100

    add cmake

---
+++ test/test.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include "tinySTL/add.h"
+
+using namespace std;
+
+int main() {
+
+    cout << "hello world" << endl;
+
+	int a = 1, b = 2;
+	cout << "a + b = " << add(a, b) << endl;
+
+    return 0;
+}
+
---
+++ tinySTL/add.h
@@ -0,0 +1,8 @@
+#ifndef TINYSTL_ADD_H_
+#define TINYSTL_ADD_H_
+
+int add(int x, int y) {
+	return x + y;
+}
+
+#endif
